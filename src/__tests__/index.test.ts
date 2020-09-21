74a153290f2f3acdde770e04ecd07d651a9e5d46
Ruben Gees <rubengees7@googlemail.com>
2020-09-21 22:30:39 +0200

    Initial commit

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,211 @@
+import { Auth } from "@nuxtjs/auth-next"
+import jwt from "jsonwebtoken"
+import plugin from "../index"
+
+jest.useFakeTimers()
+
+test("refresh token about to expire", () => {
+  const callback = jest.fn()
+
+  const auth = {
+    strategies: {
+      refresh: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return jwt.sign(
+              {
+                exp: nowSeconds + 10,
+                iat: nowSeconds - 100,
+              },
+              "secret"
+            )
+          },
+          status() {
+            return {
+              expired() {
+                return false
+              },
+            }
+          },
+        },
+      },
+    },
+    refreshTokens: callback,
+  } as any
+
+  plugin({ $auth: auth })
+
+  jest.runOnlyPendingTimers()
+
+  expect(callback).toBeCalled()
+})
+
+test("do not refresh not expiring soon", () => {
+  const callback = jest.fn()
+
+  const $auth = {
+    strategies: {
+      refresh: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return jwt.sign(
+              {
+                exp: nowSeconds + 100,
+                iat: nowSeconds - 10,
+              },
+              "secret"
+            )
+          },
+          status() {
+            return {
+              expired() {
+                return false
+              },
+            }
+          },
+        },
+      },
+    },
+    refreshTokens: callback,
+  } as any
+
+  plugin({ $auth })
+
+  jest.runOnlyPendingTimers()
+
+  expect(callback).toBeCalledTimes(0)
+})
+
+test("multiple strategies", ()=> {
+  const callback = jest.fn()
+
+  const auth = {
+    strategies: {
+      refreshIrrlevant: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return jwt.sign(
+              {
+                exp: nowSeconds + 100,
+                iat: nowSeconds - 10,
+              },
+              "secret"
+            )
+          },
+          status() {
+            return {
+              expired() {
+                return false
+              },
+            }
+          },
+        },
+      },
+      refresh: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return jwt.sign(
+              {
+                exp: nowSeconds + 10,
+                iat: nowSeconds - 100,
+              },
+              "secret"
+            )
+          },
+          status() {
+            return {
+              expired() {
+                return false
+              },
+            }
+          },
+        },
+      },
+    },
+    refreshTokens: callback,
+  } as any
+
+  plugin({ $auth: auth })
+
+  jest.runOnlyPendingTimers()
+
+  expect(callback).toBeCalled()
+})
+
+test("ignore expired token", () => {
+  const callback = jest.fn()
+
+  const auth = {
+    strategies: {
+      refresh: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return jwt.sign(
+              {
+                exp: nowSeconds + 10,
+                iat: nowSeconds - 100,
+              },
+              "secret"
+            )
+          },
+          status() {
+            return {
+              expired() {
+                return true
+              },
+            }
+          },
+        },
+      },
+    },
+    refreshTokens: callback,
+  } as any
+
+  plugin({ $auth: auth })
+
+  jest.runOnlyPendingTimers()
+
+  expect(callback).toBeCalledTimes(0)
+})
+
+test("ignore invalid token", () => {
+  const callback = jest.fn()
+
+  const auth = {
+    strategies: {
+      refresh: {
+        token: {
+          get() {
+            const nowSeconds = new Date().getTime() / 1000
+
+            return "invalid"
+          },
+          status() {
+            return {
+              expired() {
+                return false
+              },
+            }
+          },
+        },
+      },
+    },
+    refreshTokens: callback,
+  } as any
+
+  plugin({ $auth: auth })
+
+  jest.runOnlyPendingTimers()
+
+  expect(callback).toBeCalledTimes(0)
+})
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Auth, Scheme } from "@nuxtjs/auth-next"
+import jwtDecode from "jwt-decode"
+
+type AuthToken = {
+  get(): any
+}
+
+type JwtToken = {
+  exp: number
+  iat: number
+}
+
+function shouldRefresh(token: AuthToken): boolean {
+  try {
+    const decodedToken = jwtDecode<JwtToken>(token.get())
+    const timeToLive = decodedToken.exp * 1000 - decodedToken.iat * 1000
+    const timeDifference = decodedToken.exp * 1000 - new Date().getTime()
+    const timeExpired = timeToLive - timeDifference
+
+    if (timeExpired / timeToLive >= 0.75) {
+      return true
+    } else {
+      return false
+    }
+  } catch (ignored) {
+    return false
+  }
+}
+
+export default function ({ $auth }: { $auth: Auth }) {
+  setInterval(() => {
+    const strategies = $auth.strategies as { [key: string]: Scheme }
+
+    const hasTokenToRefresh = Object.values(strategies).reduce((acc, scheme) => {
+      const token = scheme.token
+
+      if (token && !token?.status()?.expired()) {
+        return acc || shouldRefresh(token)
+      } else {
+        return acc || false
+      }
+    }, false)
+
+    if (hasTokenToRefresh) {
+      $auth.refreshTokens()
+    }
+  }, 500)
+}
